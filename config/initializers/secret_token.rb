94963575a714c7b0aaed341b0b69ed222c1e091b
deepak vokaliga <amsgforme@gmail.com>
2013-04-21 09:18:06 -0400

    enabling rails for egency for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EgencyHeroku::Application.config.secret_token = '56ea305cdd0cc36c10c0dd64e2c5d7f52e009f55f93b29194d84a4d710094bd23fa5d63c25d69284bb301d1e5002328f2d8b30932f956f706c1f76ec86c06d53'
